f508d218b57f54f4f8b18265e46e7990dda7b39c
soyung sim <simsy1024@gmail.com>
2024-06-25 16:38:26 +0900

    Initial Commit

---
+++ MovieGroupApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieGroupApp
+//
+//  Created by 심소영 on 6/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
